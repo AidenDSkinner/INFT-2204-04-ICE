91eaa6f1a6422bdfdbb731e9999650140ae70b54
AidenDSkinner <aiden.skinner@dcmail.ca>
2024-01-12 23:36:11 -0500

    List.js scripts finished.
    All scripts requested to be written in list.js have been completed.

--- ICE1/scripts/list.js
+++ ICE1/scripts/list.js
@@ -1,39 +1,49 @@
 // ADDING ITEMS TO START AND END OF LIST
 // Get the <ul> element
+
 var groceryList = document.getElementById("groceries");
 
-// ADD NEW ITEM TO END OF LIST
+// ADD NEW ITEMS TO END OF LIST
 // Create element
-const newListItem = document.createElement("li");
-
+var newListItem = document.createElement("li");
 // Create text node
-const listItemContent = document.createTextNode("hotdog");
-
+var textNode = document.createTextNode("HotDog");
 // Add text node to element
-newListItem.appendChild(listItemContent);
-
+newListItem.appendChild(textNode);
 // Add element end of list
 groceryList.appendChild(newListItem);
 
-// ADD NEW ITEM START OF LIST
+// ADD NEW ITEMS START OF LIST
 // Create element
+var newListItem = document.createElement("li");
 // Create text node
+var textNode = document.createTextNode("Pizza");
 // Add text node to element
+newListItem.appendChild(textNode);
 // Add element to list
-
-
-
+groceryList.insertBefore(newListItem, groceryList.firstChild);
 // All <li> elements
-var listItems = groceryList.getElementsByTagName("li");
+var newListItem = document.createElement("li");
+
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
 // Counter variable
+var list = groceryList.children;
+var count = 0
 // Loop through elements
-// Change class to cool
+for (var i = 0; i < list.length; i++) {
+    // Change class to cool
+    list[i].setAttribute("class", "COOL");
+    count++;
+}
 
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
 // h2 element
+var page = document.getElementById("page");
 // h2 text
+var newListItem = document.createElement("h2");
 // No. of <li> elements
 // Content
 // Update h2 using innerHTML (not textContent) because it contains markup
+newListItem.innerHTML = ("Count: " + count);
+page.insertBefore(newListItem, page.firstChild);
