49b0e999764fe4b63d4954c1af9cde42a6798551
AidenDSkinner <aiden.skinner@dcmail.ca>
2024-02-20 18:10:55 -0500

    Commit 3: Fetch_script
    Fetch script code has been added from our code-along session. Everything is complete save for the final TODO at the bottom to set the output.

--- ICE6/scripts/fetch_script.js
+++ ICE6/scripts/fetch_script.js
@@ -1,16 +1,28 @@
 console.log("fetch_script.js loaded");
 
-// get the button for fetch
-
+// get the button for xhr
+let btn_fetch = $('#FetchJoke');
 // create a url variable
+let url_xhr = "https://icanhazdadjoke.com/";
 
 //need a header Accept value set to 'application/json'
-
+let myFetchHeader = {"Accept": "application/json"};
 // create the callback for the click
+$(btn_fetch).click(() => {
     // use fetch
+    fetch(url_fetch, {
         // give the necessary header data
-        // first then() to recieve the promise
-            // send the json from the promise on to the next then()
+        headers: myFetchHeader
+    })
+    // first then() to recieve the promise
+    .then((result) => {
+        // send the json from the promise on to the next then()
+        return result.json();
+        
+    })
             // console log the json
+            console.log(jsonRes);
             // set the output
+});
+
 
