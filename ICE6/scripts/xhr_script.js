d3e00149f230594e19dc3fbc4f9a4899473e1940
AidenDSkinner <aiden.skinner@dcmail.ca>
2024-02-20 17:54:07 -0500

    Commit 2: xhr_script completion
    xhr_script has had the all missing code added.

--- ICE6/scripts/xhr_script.js
+++ ICE6/scripts/xhr_script.js
@@ -1,19 +1,37 @@
 console.log("xhr_script.js loaded");
 
 // get the button for xhr
-
+let btn_Xhr = $('#xhrJoke');
 // create a url variable
-
+let url_xhr = "https://icanhazdadjoke.com/";
 // create a click callback that handles the API call
+$(btn_Xhr).click(() =>{
+
     // instantiate an XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
     // confiture with open()
+    xhr.open("GET", url_xhr);
     // set the necessary headers for icanhazdadjoke.com
+    xhr.setRequestHeader("Accept", "application/json");
     // create the callback to handle the onreadystatechange
-        // only execute when done and status is 200
-            // get the responseText
-            // console log
-            // JSON.parse the joke
-            // set the output
-    // send the request
+    xhr.onreadystatechange = function () {
+            // only execute when done and status is 200
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+                // get the responseText
+                let joke = xhr.responseText;
+                // console log
+                console.log(joke);
+                // JSON.parse the joke
+                // set the output
+                
+                
+            }
+      
+    }
+// send the request
+xhr.send();            
+
+});
+
 
 
